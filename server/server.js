c321c149a12c5a1d594ebf2d2211b8c6ede551c2
geicibarham <geicianecosta43@gmail.com>
2022-05-29 11:53:19 -0400

    changed server and auth to include graphql and fix 500 error

--- server/server.js
+++ server/server.js
@@ -6,31 +6,28 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  // context: authMiddleware,
+  introspection: process.env.NODE_ENV !== 'production'
 });
 
-const app = express();
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
-  }
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, '../client/build/index.html'))
-  })
 
   db.once('open', () => {
     app.listen(PORT, () => {
@@ -39,6 +36,12 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
 };
+
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(_dirname, '../client/build/index.html'))
+  // })
+
+
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
